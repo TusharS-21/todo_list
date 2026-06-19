2021fdaecbdcd336b1eda40a4593f67f90814e76
Tushar Valand <tusharsharma6129@gmail.com>
2026-06-19 20:04:58 +0530

    added navbar

--- script.js
+++ script.js
@@ -178,4 +178,11 @@ todoList.addEventListener('click', event => {
   if (action === 'remove') deleteTodo(id);
 });
 
+// Mobile nav toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+if (navToggle && navLinks) {
+  navToggle.addEventListener('click', () => navLinks.classList.toggle('open'));
+}
+
 loadTodos();
